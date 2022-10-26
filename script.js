45290b5845746a30c4a0f737ddf1e6dcef024d1a
taskotale <110062804+taskotale@users.noreply.github.com>
2022-10-25 21:33:03 -0400

    adjusted bug for moving button when changing og pass

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const fName = document.getElementById('first-name');
-const lName = document.getElementById('last-name');
-const email = document.getElementById('email');
-const phoneNum = document.getElementById('phone-number');
-const password = document.getElementById('password');
+const fNameInput = document.getElementById('first-name');
+const lNameInput = document.getElementById('last-name');
+const emailInput = document.getElementById('email');
+const phoneNumInput = document.getElementById('phone-number');
+const passwordInput = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
 const submitBtn = document.getElementById('submit-btn')
 
-
-const btnJump = (buttonToMove) => {
-    if (buttonToMove.classList.contains('move')) {
-        buttonToMove.classList.remove('move')
-    } else { buttonToMove.classList.add('move') }
-}
+let passSame = false;
 
 submitBtn.addEventListener('mouseover', e => btnJump(submitBtn))
 
-const checkPassword = (origPass, needConfirm, someButton) => {
-    someButton.addEventListener('mouseover', e => btnJump(someButton))
-    if (needConfirm.value === origPass.value) {
-        someButton.removeEventListener('mouseover', e => btnJump(someButton),)
+const btnJump = (buttonToMove) => {
+    if (!passSame) {
+        if (buttonToMove.classList.contains('move')) {
+            buttonToMove.classList.remove('move')
+        } else { buttonToMove.classList.add('move') }
+    } else {
+        buttonToMove.removeEventListener('mouseover', e => btnJump(submitBtn))
     }
 }
 
-confirmPassword.addEventListener('keyup', e => checkPassword(password, confirmPassword, submitBtn))
+const checkPassword = (origPass, needConfirm) => {
+    if (needConfirm.value === origPass.value) {
+        return passSame = true;
+    } else {
+        return passSame = false
+    }
+}
+
+passwordInput.addEventListener('keyup', e => checkPassword(confirmPassword, passwordInput))
+confirmPassword.addEventListener('keyup', e => checkPassword(passwordInput, confirmPassword))
 
